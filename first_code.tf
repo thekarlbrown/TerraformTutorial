a8b619e174900d7870b9968806e31295b1c9f766
Karl Brown <karlbrownthinks@gmail.com>
2022-10-05 23:16:48 -0400

    first bucket successful

--- first_code.tf
+++ first_code.tf
@@ -1,9 +1,14 @@
 provider "aws" {
     profile = "default"
     region  = "us-east-1"
+    shared_credentials_files = ["~/Projects/TerraformTutorial/credentials"]
 }
 
 resource "aws_s3_bucket" "tf_course" {
+    bucket = "karls-tf-tutorial-bucket"
+}
+
+resource "aws_s3_bucket_acl" "tf_course" {
     bucket = "karls-tf-tutorial-bucket"
     acl = "private"
 }
