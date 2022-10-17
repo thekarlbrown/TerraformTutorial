dddc866f088ac64d156e4653948f47f47c0ba22a
Karl Brown <karlbrownthinks@gmail.com>
2022-10-17 13:35:28 -0400

    added elastic ip

--- first_code.tf
+++ first_code.tf
@@ -61,3 +61,16 @@ resource "aws_instance" "prod_web" {
         "Terraform": "true"
     }
 }
+
+resource "aws_eip_association" "prod_web" {
+    instance_id = aws_instance.prod_web.id
+    allocation_id = aws_eip.prod_web.id
+}
+
+resource "aws_eip" "prod_web" {
+    instance = aws_instance.prod_web.id
+
+    tags = {
+        "Terraform": "true"
+    }
+}
