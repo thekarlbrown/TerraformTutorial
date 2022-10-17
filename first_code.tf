62ffa23b0ff920f80ca18fc665fb36ece317d209
Karl Brown <karlbrownthinks@gmail.com>
2022-10-17 13:53:52 -0400

    load balancer added

--- first_code.tf
+++ first_code.tf
@@ -19,6 +19,22 @@ resource "aws_default_vpc" "default" {
     }
 }
 
+resource "aws_default_subnet" "default_az1" {
+    availability_zone = "us-east-1a"
+    tags = {
+        "Terraform": "true"
+    }
+ }
+
+resource "aws_default_subnet" "default_az2" {
+    availability_zone = "us-east-1b"
+    tags = {
+        "Terraform": "true"
+    }
+ }
+
+
+
 resource "aws_security_group" "prod_web" {
     name = "prod_web"
     description = "Allow standard HTTP(S) ports inbound and everything outbound"
@@ -50,6 +66,8 @@ resource "aws_security_group" "prod_web" {
 }
 
 resource "aws_instance" "prod_web" {
+    count = 2
+
     ami = "ami-0b73f70247c2526d6"
     instance_type = "t2.nano"
 
@@ -63,12 +81,30 @@ resource "aws_instance" "prod_web" {
 }
 
 resource "aws_eip_association" "prod_web" {
-    instance_id = aws_instance.prod_web.id
+    instance_id = aws_instance.prod_web.0.id
     allocation_id = aws_eip.prod_web.id
 }
 
 resource "aws_eip" "prod_web" {
-    instance = aws_instance.prod_web.id
+    instance = aws_instance.prod_web.0.id
+
+    tags = {
+        "Terraform": "true"
+    }
+}
+
+resource "aws_elb" "prod_web" {
+    name = "prod-web"
+    instances = aws_instance.prod_web.*.id
+    subnets = [ aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id ]
+    security_groups = [ aws_security_group.prod_web.id ]
+
+    listener {
+      instance_port = 80
+      instance_protocol = "http"
+      lb_port = 80
+      lb_protocol = "http"
+    }
 
     tags = {
         "Terraform": "true"
