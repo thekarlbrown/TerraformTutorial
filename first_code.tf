356a18a9a5f13a544c1e922a50f7df5e1840993b
Karl Brown <karlbrownthinks@gmail.com>
2022-10-17 12:50:32 -0400

    defined initial web security group

--- first_code.tf
+++ first_code.tf
@@ -18,3 +18,33 @@ resource "aws_default_vpc" "default" {
         Name = "Default VPC"
     }
 }
+
+resource "aws_security_group" "prod_web" {
+    name = "prod_web"
+    description = "Allow standard HTTP(S) ports inbound and everything outbound"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = [ "151.200.235.163/32" ]
+    }
+
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = [ "151.200.235.163/32" ]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    tags = {
+        "Terraform": "true"
+    }
+}
